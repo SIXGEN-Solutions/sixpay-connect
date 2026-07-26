1b7160db20dbc25a7ce92a8cbf6f7445836a1c6c
suvorov <suvorov22002@gmail.com>
2026-07-26 10:35:58 -0400

    backend foundation - parent pom bom modules

---
+++ backend/bootstrap/src/main/java/com/sixpay/bootstrap/BootStrapApplication.java
@@ -0,0 +1,13 @@
+package com.sixpay.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStrapApplication.class, args);
+    }
+
+}
