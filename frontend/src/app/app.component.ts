500e9b265644b8f38d761c1aac429dc15cfcc35f
suvorov <suvorov22002@gmail.com>
2026-07-28 15:28:06 -0400

    audit et cadrage fix build migrate to angular 21 install material cdk

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare module 'tslib';
+
 @Component({
-  selector: 'app-root',
+  selector: 'sp-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
