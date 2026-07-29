a1e7023a600397b9d40217ff8407c46f7eb4d088
suvorov <suvorov22002@gmail.com>
2026-07-28 21:46:04 -0400

    feat: ajout du proxy config

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ import { AppEnvironment } from './environment.model';
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: '',
 } satisfies AppEnvironment;
